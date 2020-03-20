6966c95e89eed9014b2bfc302eff09d037ed1652
chris-unbolted <chris@unbolted.com>
2020-03-20 10:25:01 +0000

    Add explicit on_delete on ForeignKey field.

--- referral/models.py
+++ referral/models.py
@@ -34,7 +34,7 @@ class Referrer(models.Model):
     name = models.CharField(_("Name"), max_length=255, unique=True)
     description = models.TextField(_("Description"), blank=True, null=True)
     creation_date = models.DateTimeField(_("Creation date"), auto_now_add=True)
-    campaign = models.ForeignKey(Campaign, verbose_name=_("Campaign"), related_name='referrers', blank=True, null=True)
+    campaign = models.ForeignKey(Campaign, verbose_name=_("Campaign"), related_name='referrers', blank=True, null=True, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['name']
